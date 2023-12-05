6cba9e2847f1e442537aedbc2f3858427d1ab279
chebupelka <119750562+chebupelka8@users.noreply.github.com>
2023-12-06 01:15:45 +0500

    Add files via upload

--- source/scripts/tab_editor.py
+++ source/scripts/tab_editor.py
@@ -10,18 +10,27 @@ class TabEditorArea(QTabWidget):
 
         self.addTab(WelcomePage(self), "Welcome")
         self.setStyleSheet(load_style("source/gui/style/tab_editor.css"))
+        self.setTabsClosable(True)
     
     def _add_editor(self, __name: str, __text: str, full_path: str = None) -> None:
         self.codeArea = CodeEditorArea(self)
         self.codeArea.insertPlainText(__text)
         self.codeArea.setCurrentPath(full_path)
 
-        if self._get_by_name(__name) == None: self.addTab(self.codeArea, __name)
+        if self._get_count_by_name(__name) == None: self.addTab(self.codeArea, __name)
     
-    def _get_by_name(self, __name) -> int | None:
+    def _get_count_by_name(self, __name) -> int | None:
         res = 0
 
         for w in range(self.count()):
             if __name == self.tabText(w): res += 1
         
         return res if res != 0 else None
+
+    def _get_index_by_name(self, __name) -> int | None:
+        index = None
+
+        for w in range(self.count()):
+            if __name == self.tabText(w): index = self.indexOf(self.widget(w))
+        
+        return index
